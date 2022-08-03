abd2b20ad24b370fc3120692d67529ad8130b31f
Annie Hosch <anniehosch@Annies-Air.lan>
2022-08-02 23:50:03 -0400

    added assoication methods for seq models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,4 +1,5 @@
 //NOTE: replace the index.js file in the Develop/models folder with this one.  
+//remember foreign keys
 
 // import models
 const Product = require('./Product');
@@ -7,8 +8,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
