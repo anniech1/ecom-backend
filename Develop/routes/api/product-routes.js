8806abb43c39ddf012bce8548c180c59bbc8a2b9
anniech1 <ach577@nyu.edu>
2022-09-06 11:13:12 -0400

    fixed product delete route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -124,22 +124,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  try {
-    const productData = await Product.destroy({
-      where: {
-        id: req.params.id,
-      },
+  Product.destroy({
+    where: {id:req.params.id}
+  })
+  .then((data) => res.json(data))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
     });
-
-    if (!productData) {
-      res.status(404).json({ message: 'No products found with that id!' });
-      return;
-    }
-
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
